ffab74d3aa29aaf25b21a0c0fb9114f10b2b9454
kwantam <kwantam@gmail.com>
2014-04-15 07:35:27 -0400

    char[] backing for alert, clear char[] in onPause

--- src/org/jfet/batsPass/batsPassMain.java
+++ src/org/jfet/batsPass/batsPassMain.java
@@ -1,12 +1,17 @@
 package org.jfet.batsPass;
 
 import java.io.File;
+import java.nio.CharBuffer;
+import java.util.Arrays;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.CharArrayBuffer;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
@@ -19,9 +24,7 @@ import net.sqlcipher.Cursor;
 import net.sqlcipher.database.SQLiteDatabase;
 import net.sqlcipher.database.SQLiteException;
 
-public class batsPassMain extends Activity
-{
-	private boolean didLoad = false;
+public class batsPassMain extends Activity {
 	private SQLiteDatabase passDB = null;
 	
     public void onCreate(Bundle savedInstanceState) {
@@ -40,15 +43,9 @@ public class batsPassMain extends Activity
     		thePass[i] = passEdit.charAt(i);
     		passEdit.replace(i,i+1,"Z");
     	}
-    	// now that we've overwritten the bytes in memory
-    	// (we hope... the GC will fight us on this), set
-    	// the length to zero
     	passEdit.clear();
     	
-    	if (! didLoad) {
-    		didLoad = true;
-    		SQLiteDatabase.loadLibs(this);
-    	}
+    	SQLiteDatabase.loadLibs(this);
     	
     	final File databaseFile = getDatabasePath("password.db");
     	final boolean fExists = databaseFile.exists();
@@ -119,17 +116,35 @@ public class batsPassMain extends Activity
         }
         
         c.moveToFirst();
-        final String service = c.getString(0);
-        final String uid = c.getString(1);
-        final String pw = c.getString(2);
+        final char[] dummy = new char[0];
+        final CharArrayBuffer service = new CharArrayBuffer(dummy);
+        final CharArrayBuffer uid = new CharArrayBuffer(dummy);
+        final CharArrayBuffer pw = new CharArrayBuffer(dummy);
+        c.copyStringToBuffer(0,service);
+        c.copyStringToBuffer(1,uid);
+        c.copyStringToBuffer(2,pw);
         c.close();
         
-        final DialogFragment dlg = new PassDlg() {
-            protected void setData(AlertDialog.Builder ab) {
-                ab.setTitle(service);
-                ab.setMessage("Username:\n" + uid + "\n\nPassword:\n" + pw);
-            }
-        };
+        final char[] tmp1 = "Username:\n".toCharArray();
+        final char[] tmp2 = "\n\nPassword:\n".toCharArray();
+        final char[] msg = new char[uid.data.length + pw.data.length + tmp1.length + tmp2.length];
+        
+        for (int i=0; i<tmp1.length; i++) {
+            msg[i] = tmp1[i];
+        }
+        for (int i=0; i<uid.data.length; i++) {
+            msg[tmp1.length + i] = uid.data[i];
+            uid.data[i] = 'Z';
+        }
+        for (int i=0; i<tmp2.length; i++) {
+            msg[tmp1.length + uid.data.length + i] = tmp2[i];
+        }
+        for (int i=0; i<pw.data.length; i++) {
+            msg[tmp1.length + uid.data.length + tmp2.length + i] = pw.data[i];
+            pw.data[i] = 'Z';
+        }
+        
+        final DialogFragment dlg = new PassDlg(msg,service.data);
         dlg.show(getFragmentManager(), "ZZZZZZZZ");
     }
     
@@ -143,16 +158,37 @@ public class batsPassMain extends Activity
         }
     }
     
-    public static abstract class PassDlg extends DialogFragment {
-        protected abstract void setData(AlertDialog.Builder ab);
+    @SuppressLint("ValidFragment")
+    public static class PassDlg extends DialogFragment {
+        final char[] msg;
+        final char[] title;
+        
+        public PassDlg (char[] msg, char[] title) {
+            super();
+            this.msg = msg;
+            this.title = title;
+        }
+        
+        public PassDlg () {
+            super();
+            this.msg = "".toCharArray();
+            this.title = "".toCharArray();
+        }
         
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             final AlertDialog.Builder ab = new AlertDialog.Builder(getActivity());
             ab.setNegativeButton(R.string.done_string, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface d, int i) { PassDlg.this.getDialog().cancel(); }
             });
-            setData(ab);
+            ab.setTitle(CharBuffer.wrap(title));
+            ab.setMessage(CharBuffer.wrap(msg));
             return ab.create();
+        }
+        
+        public void onPause() {
+            super.onPause();
+            Arrays.fill(msg, 'Z');
+            Arrays.fill(title, 'Z');
         }
     }
     
